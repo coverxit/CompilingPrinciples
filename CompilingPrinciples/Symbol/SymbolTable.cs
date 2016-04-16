6f4d67ff3ab79387685a2904e007edaaa6ebdf81
Shindo Wu <cn.shindo@gmail.com>
2016-04-16 13:29:17 +0800

    Add files

---
+++ CompilingPrinciples/Symbol/SymbolTable.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Lex;
+
+namespace Symbol
+{
+    public class SymbolTableEntry
+    {
+        private string lexeme;
+        private Tag tag;
+
+        public String Lexeme
+        {
+            get { return lexeme; }
+        }
+
+        public Tag Tag
+        {
+            get { return tag; }
+        }
+
+        public SymbolTableEntry(string lexeme, Tag tag)
+        {
+            this.lexeme = lexeme;
+            this.tag = tag;
+        }
+
+        public SymbolTableEntry(SymbolTableEntry other)
+        {
+            this.lexeme = other.lexeme;
+            this.tag = other.tag;
+        }
+    }
+
+    public class SymbolTable
+    {
+        private List<SymbolTableEntry> symbols;
+
+        public SymbolTable()
+        {
+            symbols = new List<SymbolTableEntry>();
+        }
+
+        public SymbolTable(SymbolTable rhs)
+        {
+            symbols = new List<SymbolTableEntry>(rhs.symbols);
+        }
+
+        public int AddSymbol(string symbol, Tag tag)
+        {
+            lock (symbols)
+            {
+                symbols.Add(new SymbolTableEntry(symbol, tag));
+                return symbols.Count - 1;
+            }
+        }
+
+        public string GetSymbol(int index)
+        {
+            // Debug.Assert(index >= 0 && index < symbols.Count, "Index Overflow");
+            return symbols[index].Lexeme;
+        }
+
+        public Tuple<SymbolTableEntry, int> GetSymbolEntry(string symbol)
+        {
+            for (int i = 0; i < symbols.Count; i++)
+                if (symbols[i].Lexeme == symbol)
+                    return Tuple.Create<SymbolTableEntry, int>(new SymbolTableEntry(symbols[i]), i);
+
+            return null;
+        }
+
+        public List<SymbolTableEntry> ToList()
+        {
+            return new List<SymbolTableEntry>(symbols);
+        }
+    }
+}
---
+++ ParseTableGenerator/Grammar/Production.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParseTableGenerator
+{ 
+    public class Production
+    {
+        private Grammar grammar;
+
+        private int leftNonTerminalId;
+        public ProductionSymbol Left
+        {
+            get { return new ProductionSymbol(grammar, ProductionSymbol.SymbolType.NonTerminal, leftNonTerminalId); }
+        }
+
+        private List<ProductionSymbol> rightExpression;
+        public List<ProductionSymbol> Right
+        {
+            get { return new List<ProductionSymbol>(rightExpression); }
+        }
+
+        public Production(Grammar grammar)
+        {
+            this.grammar = grammar;
+            rightExpression = new List<ProductionSymbol>();
+        }
+
+        public Production(Production rhs)
+        {
+            this.grammar = rhs.grammar;
+            this.leftNonTerminalId = rhs.leftNonTerminalId;
+            this.rightExpression = new List<ProductionSymbol>(rhs.rightExpression);
+        }
+
+        public void SetLeftNonTerminal(int id)
+        {
+            this.leftNonTerminalId = id;
+        }
+
+        public void AppendRightSymbol(ProductionSymbol.SymbolType type, int id)
+        {
+            rightExpression.Add(new ProductionSymbol(grammar, type, id));
+        }
+
+        public override int GetHashCode()
+        {
+            return this.ToString().GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+                return false;
+
+            Production rhs = obj as Production;
+            return rhs.grammar == grammar && rhs.ToString() == this.ToString();
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(Left);
+            sb.Append(" ->");
+
+            foreach (var e in rightExpression)
+            {
+                sb.Append(" ");
+                sb.Append(e);
+            }
+            
+            return sb.ToString();
+        }
+    }
+}
