9eb00adccb6afb180f1a56151436561e08062b7a
Shindo Wu <cn.shindo@gmail.com>
2016-04-16 17:54:57 +0800

    Add files

---
+++ ParseGenerator/CLR/LR1Collection.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParseGenerator
+{
+    class LR1Collection
+    {
+        private Grammar grammar;
+        private FirstFollowSet firstSet;
+
+        public LR1Collection(Grammar grammar)
+        {
+            this.grammar = grammar;
+            firstSet = new FirstFollowGenerator(grammar).FirstSet;
+        }
+
+        public HashSet<LR1Item> Closure(HashSet<LR1Item> I)
+        {
+            var closure = new HashSet<LR1Item>(I);
+            var computeStack = new Stack<LR1Item>();
+            
+            // Push each [A -> α·Bβ, a] into stack
+            foreach (var item in closure.Where(e => e.SymbolAfterDot.Type == ProductionSymbol.SymbolType.NonTerminal))
+                computeStack.Push(item);
+            
+            while (computeStack.Count > 0)
+            {
+                var topItem = computeStack.Pop();
+
+                // Compute First(βa) first
+                // We genrate a sequence { β, a }
+                var sequence = new List<ProductionSymbol>();
+                sequence.AddRange(topItem.Production.Right.Skip(topItem.DotPosition + 1));
+                sequence.Add(topItem.Lookahead);
+
+                var firstBetaA = new HashSet<ProductionSymbol>();
+                foreach (var e in sequence)
+                    if (e.Type == ProductionSymbol.SymbolType.Terminal)
+                    {
+                        firstBetaA.Add(e);
+                        break;
+                    }
+                    else
+                    {
+                        firstBetaA.UnionWith(firstSet.Get(e));
+                        if (!firstSet.Get(e).Contains(grammar.Epsilon))
+                            break;
+                    }
+
+                // Each production B -> γ
+                foreach (var prod in grammar.Productions.Where(e => e.Left.Equals(topItem.SymbolAfterDot)))
+                    // Each terminal b in First(βa), add [B -> ·γ, b]
+                    foreach (var b in firstBetaA)
+                    {
+                        // Add [B -> ·γ, b] into closure
+                        var item = new LR1Item(grammar, prod, 0, b);
+                        
+                        // If B -> ·γ is something like B -> ·Cβ, then push into stack
+                        if (!closure.Contains(item) && item.SymbolAfterDot.Type == ProductionSymbol.SymbolType.NonTerminal)
+                            computeStack.Push(item);
+
+                        closure.Add(item);
+                    }
+
+            }
+
+            return closure;
+        }
+
+        public HashSet<LR1Item> Goto(HashSet<LR1Item> I, ProductionSymbol X)
+        {
+            var gotoSet = new HashSet<LR1Item>();
+            foreach (var item in I.Where(e => e.SymbolAfterDot.Equals(X)))
+                gotoSet.Add(new LR1Item(grammar, item.Production, item.DotPosition + 1, item.Lookahead));
+            return Closure(gotoSet);
+        }
+
+        public HashSet<HashSet<LR1Item>> Items()
+        {
+            var collection = new HashSet<HashSet<LR1Item>>();
+            var computeStack = new Stack<HashSet<LR1Item>>();
+
+            // C = { CLOSURE( {[S' -> ·S, $]} ) }
+            var I0 = Closure(new HashSet<LR1Item>(new LR1Item[] { new LR1Item(grammar, grammar.Productions[0], 0, grammar.EndMarker) }));
+            collection.Add(I0);
+            computeStack.Push(I0);
+
+            while (computeStack.Count > 0)
+            {
+                var topSet = computeStack.Pop();
+
+                // For each grammaer symbol X
+                foreach (var sym in grammar.Symbols)
+                {
+                    var gotoSet = Goto(topSet, sym);
+
+                    // If GOTO(I,X) not null and not in C
+                    if (gotoSet.Count > 0 && collection.Where(e => e.SetEquals(gotoSet)).Count() == 0)
+                    {
+                        // Add GOTO(I,X) to C
+                        collection.Add(gotoSet);
+                        computeStack.Push(gotoSet);
+                    }
+                }
+            }
+
+            return collection;
+        }
+    }
+}
---
+++ ParseGenerator/CLR/LR1Item.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParseGenerator
+{
+    class LR1Item : LR0Item
+    {
+        private ProductionSymbol lookahead;
+
+        public ProductionSymbol Lookahead
+        {
+            get { return lookahead; }
+        }
+
+        public LR1Item(Grammar grammar, Production prod, int dotPos, ProductionSymbol lookahead) : base(grammar, prod, dotPos)
+        {
+            this.lookahead = lookahead;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.ToString().GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj) && lookahead.Equals((obj as LR1Item).lookahead);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("[" + production.Left + " ->");
+
+            foreach (var e in production.Right.Select((value, index) => new { index, value }))
+            {
+                sb.Append(" ");
+                if (dotPos == e.index) sb.Append("·");
+                sb.Append(e.value);
+            }
+
+            // Dot is at the right end
+            if (dotPos == production.Right.Count) sb.Append("·");
+            sb.Append(", " + lookahead.ToString() + "]");
+            return sb.ToString();
+        }
+    }
+}
