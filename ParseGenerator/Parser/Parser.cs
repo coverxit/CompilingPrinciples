0d33cdbc5c2fc07be1b8f758745eeccd6b1adb66
Shindo Wu <cn.shindo@gmail.com>
2016-04-16 18:04:33 +0800

    Add files

---
+++ ParseGenerator/Parser/Parser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ParseGenerator
+{
+    public class Parser
+    {
+        private ParseTable table;
+        
+        public void Parse(Stream input)
+        {
+            // Let a be the first symbol of w$
+            int nextByte = input.ReadByte();
+            while (true)
+            {
+
+            }
+        }
+    }
+}
