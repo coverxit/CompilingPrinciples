5e0bf4d154afdeaa8d2395d684ecffb4c370d83d
Shindo Wu <cn.shindo@gmail.com>
2016-04-21 23:11:45 +0800

    .

---
+++ TestCase/ParserTest/SLRParserErrorRoutine.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using CompilingPrinciples.LexicalAnalyzer;
+using CompilingPrinciples.SymbolEnvironment;
+using CompilingPrinciples.SyntaxAnalyzer;
+
+namespace CompilingPrinciples.TestCase
+{
+    public class SLRParserErrorRoutine : IParserErrorRoutine
+    {
+        public string ErrorRoutine(int state, ProductionSymbol symbol, Token previousToken, Stack<int> parseStack, SymbolStack symbolStack)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
