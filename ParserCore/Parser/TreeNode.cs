5ad48911a8e51eafec045181a4baeed3a604ae00
Shindo Wu <cn.shindo@gmail.com>
2016-05-22 23:32:21 +0800

    add treenode

---
+++ ParserCore/Parser/TreeNode.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompilingPrinciples.ParseCore
+{
+    public class TreeNode<T>
+    {
+        private readonly T value;
+        private readonly List<TreeNode<T>> children = new List<TreeNode<T>>();
+
+        public TreeNode(T value)
+        {
+            this.value = value;
+        }
+
+        public TreeNode<T> this[int i]
+        {
+            get { return children[i]; }
+        }
+
+        public TreeNode<T> Parent { get; private set; }
+
+        public T Value
+        {
+            get { return value; }
+        }
+
+        public IReadOnlyCollection<TreeNode<T>> Children
+        {
+            get { return children.AsReadOnly(); }
+        }
+
+        public TreeNode<T> AddChild(T value)
+        {
+            var node = new TreeNode<T>(value) { Parent = this };
+            children.Add(node);
+            return node;
+        }
+
+        public TreeNode<T>[] AddChildren(params T[] values)
+        {
+            return values.Select(AddChild).ToArray();
+        }
+
+        public bool RemoveChild(TreeNode<T> node)
+        {
+            return children.Remove(node);
+        }
+
+        public void Traverse(Action<T> action)
+        {
+            action(Value);
+            foreach (var child in children)
+                child.Traverse(action);
+        }
+
+        public IEnumerable<T> Flatten()
+        {
+            return new[] { Value }.Union(children.SelectMany(x => x.Flatten()));
+        }
+    }
+}
