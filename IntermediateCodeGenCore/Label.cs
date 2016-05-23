c8a1616d8f61a181298fae02e9037e3ade7989df
Shindo Wu <cn.shindo@gmail.com>
2016-05-23 10:37:30 +0800

    add label and tempvar

---
+++ IntermediateCodeGenCore/Label.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompilingPrinciples.IntermediateCodeGenCore
+{
+    public class Label
+    {
+        private int no;
+
+        public int Number
+        {
+            get { return no; }
+        }
+
+        public Label(int no)
+        {
+            this.no = no;
+        }
+
+        public override string ToString()
+        {
+            return "L" + no + ":";
+        }
+    }
+
+    public class LabelManager
+    {
+        private int count = 0;
+
+        public Label Create()
+        {
+            return new Label(++count);
+        }
+    }
+}
---
+++ IntermediateCodeGenCore/TempVar.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompilingPrinciples.IntermediateCodeGenCore
+{
+    public class TempVar
+    {
+        private int no;
+
+        public int Number
+        {
+            get { return no; }
+        }
+
+        public TempVar(int no)
+        {
+            this.no = no;
+        }
+
+        public override string ToString()
+        {
+            return "t" + no;
+        }
+    }
+
+    public class TempVarManager
+    {
+        private int count = 0;
+
+        public TempVar Create()
+        {
+            return new TempVar(++count);
+        }
+    }
+}
